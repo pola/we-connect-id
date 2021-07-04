36d0c7cf2aba0c245db047a192d90f1b78557982
Martin Pola <martin@kottnet.net>
2021-07-04 19:16:35 +0200

    Code clean-up and linting

--- wci.py
+++ wci.py
@@ -1,6 +1,10 @@
-import random, re, requests, string
+import random
+import re
+import requests
+import string
 
 BASE_URL = 'https://mobileapi.apps.emea.vwapps.io'
+
 
 class WeConnectId:
     def __init__(self, email_address, password):
@@ -9,16 +13,16 @@ class WeConnectId:
 
         self._setup_session()
         self._sign_in()
-    
+
     def _setup_session(self):
         self.session = requests.session()
 
         self.session.headers.update({
             'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36',
         })
-    
+
     def _sign_in(self):
-        nonce = (''.join(random.choices(string.ascii_uppercase + string.ascii_lowercase + string.digits, k=16)))
+        nonce = ''.join(random.choices(string.ascii_uppercase + string.ascii_lowercase + string.digits, k=16))
 
         r = self.session.get('https://login.apps.emea.vwapps.io/authorize?nonce=' + nonce + '&redirect_uri=weconnect://authenticated')
 
@@ -55,14 +59,14 @@ class WeConnectId:
                 'https://identity.vwgroup.io/signin-service/v1/' + uuid + '@apps_vw-dilab_com/login/authenticate',
                 data=post_data
             )
-        
+
         except requests.exceptions.InvalidSchema as e:
             # We expect a redirect to 'weconnect://authenticated', which requests doesn't understand
             access_token = re.search('access_token=([^&$]+)', str(e)).group(1)
-        
+
         self.session.headers.update({
             'Authorization': 'Bearer ' + access_token,
         })
-    
+
     def get(self, endpoint):
         return self.session.get(BASE_URL + endpoint).json()
